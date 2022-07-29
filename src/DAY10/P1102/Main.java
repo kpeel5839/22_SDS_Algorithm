f806729d1031d59d31e05bacfa02fc196aae9d2a
jaeyeon kim <jakind@naver.com>
2022-07-29 22:14:27 +0900

    SDS 마지막 문제 생성

--- src/DAY10/P1102/Main.java
+++ src/DAY10/P1102/Main.java
@@ -6,9 +6,76 @@ import java.io.*;
 // Samsung SDS Practice
 
 public class Main {
+    static int N;
+    static int P;
+    static int INF = 1000;
+    static int[] dp;
+    static int[][] map;
+
+    static int dfs(int bit, int on) { // bit == 현재 켜져있는 것들, on == 켜져있는 발전소의 개수
+//        System.out.println(on);
+        if (on == P) {
+            return 0;
+        } // 다 켜진 경우 끝내준다.
+
+        if (dp[bit] != -1) {
+            return dp[bit];
+        }
+
+        int ret = INF;
+
+        for (int i = 0; i < N; i++) {
+            if ((bit & (1 << i)) == 0) {
+                // 아직 안 켜져 있는 경우
+                // 여기서 이제 킬 수 있는 것들을 다 켜줄 것임
+                // 현재 bit 에서 봤을 때, 안 켜져 있는 애들을 순서대로 진행하는 것이니까
+                // 현재 켜져 있는 애들 중에서 가장 작은 값으로 할 수 있는 애를 찾는다.
+                int min = INF;
+
+                for (int j = 0; j < N; j++) {
+                    if ((bit & (1 << j)) != 0) {
+                        System.out.println("fucking!");
+                        min = Math.min(min, map[j][i]);
+                    }
+                }
+
+                ret = Math.min(ret, dfs((bit | 1 << i), on + 1) + min);
+            }
+        }
+
+        dp[bit] = ret;
+        return dp[bit];
+    }
+
     public static void main(String[] args) throws IOException {
         System.setIn(new FileInputStream("src/DAY10/P1102/input.txt"));
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st;
+
+        N = Integer.parseInt(br.readLine());
+        map = new int[N][N];
+        dp = new int[1 << N];
+        Arrays.fill(dp, -1);
+
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < N; j++) {
+                map[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+
+        String string = br.readLine();
+        P = Integer.parseInt(br.readLine());
+        int bit = 0;
+        int on = 0;
+
+        for (int i = 0; i < N; i++) {
+            if (string.charAt(i) == 'Y') {
+                bit |= (1 << i);
+                on++;
+            }
+        }
+
+        System.out.println(dfs(bit, on));
     }
 }
--- src/DAY10/P11062/Main.java
+++ src/DAY10/P11062/Main.java
@@ -6,9 +6,48 @@ import java.io.*;
 // Samsung SDS Practice
 
 public class Main {
+    static int[] card;
+    static int[][] dp;
+
+    static int dfs(int left, int right, int turn) {
+        if (left == right) {
+            return turn % 2 == 1 ? card[left] : 0;
+        }
+
+        if (dp[left][right] != 0) {
+            return dp[left][right];
+        }
+
+        if (turn % 2 == 1) { // 근우의 턴
+            dp[left][right] = Math.max(card[left] + dfs(left + 1, right, turn + 1), card[right] + dfs(left, right - 1, turn + 1));
+        } else { // 명우의 턴
+            dp[left][right] = Math.min(dfs(left + 1, right, turn + 1), dfs(left, right - 1, turn + 1));
+        }
+
+        return dp[left][right];
+    }
+
     public static void main(String[] args) throws IOException {
         System.setIn(new FileInputStream("src/DAY10/P11062/input.txt"));
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
         StringTokenizer st;
+
+        int T = Integer.parseInt(br.readLine());
+
+        while (T-- > 0) {
+            int N = Integer.parseInt(br.readLine());
+            st = new StringTokenizer(br.readLine());
+            dp = new int[N][N]; // left, right 로 현재 남은 카드 범위를 나타내기 위한 dp 이다, 선택하지 않는 경우는 없으니까, 0이 아직 방문을 하지 않았다는 증표이다.
+            card = new int[N];
+
+            for (int i = 0; i < N; i++) {
+                card[i] = Integer.parseInt(st.nextToken());
+            }
+
+            sb.append(dfs(0, N - 1, 1)).append("\n");
+        }
+        
+        System.out.print(sb);
     }
 }
